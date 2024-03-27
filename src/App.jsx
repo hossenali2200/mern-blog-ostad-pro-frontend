27af79d9fb020a68a20e39c579169e57c22942c2
hossenali2200 <hosenali2200@gmail.com>
2024-03-27 18:05:08 +0600

    add delete signout
    Add delete and signout function and admin function

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import PrivateRoute from './components/PrivateRoute'
+import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute'
+import CreatePost from './pages/CreatePost'
 
 function App() {
    
@@ -17,10 +20,17 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
-          <Route path='/dashboard' element={<Dashboard />} />
-          <Route path='/projects' element={<Projects />} />
           <Route path='/signup' element={<SignUp />} />
           <Route path='/signin' element={<SignIn/>} />
+          <Route element={<PrivateRoute/>}>
+                <Route path='/dashboard' element={<Dashboard />} /> 
+          </Route>
+          <Route element={<OnlyAdminPrivateRoute/>}>
+                <Route path='/create-post' element={<CreatePost />} /> 
+          </Route>
+          
+          <Route path='/projects' element={<Projects />} />
+         
         </Routes>
         <Footer/>
     </BrowserRouter>
